093a5ec096f5cc1e2b729880eabd85aea3d4bbf5
Valentin Zadorozhniy <someaderss3000@gmail.com>
2024-10-14 06:48:08 +0700

    add foreign key

--- DDL/DDL.md
+++ DDL/DDL.md
@@ -3,13 +3,13 @@
 - ### Create
   - `CREATE DATABASE company_repository;` - создание новой БД
   - `CREATE SCHEMA company_storage;` - создание новой схемы в БД
-  - `CREATE TABLE company("id" INT GENERATED ALWAYS AS IDENTITY, "name" VARCHAR(128) UNIQUE NOT NULL, manager VARCHAR(128) REFERENCES employee (id), "date" DATE NOT NULL CHECK (date > '1995-01-01' AND date < '2020-01-01'), PRIMARY KEY (id) , UNIQUE(name, date));` - создание новой таблицы с полями в БД
+  - `CREATE TABLE company("id" INT GENERATED ALWAYS AS IDENTITY, "name" VARCHAR(128) UNIQUE NOT NULL, manager VARCHAR(128) REFERENCES employee (id), "date" DATE NOT NULL CHECK (date > '1995-01-01' AND date < '2020-01-01'), PRIMARY KEY (id) , UNIQUE(name, date), , FOREIGN KEY (company_name) REFERENCES company(name) ON DELETE CASCADE));` - создание новой таблицы с полями в БД
      - `GENERATED ALWAYS AS IDENTITY` - автоинкремент ключа
      - `UNIQUE` - уникальный ключ
      - `NOT NULL` - не null
      - `CHECK (...)` - проверка значения на условие 
      - `PRIMARY KEY` - основной ключ
-     - `REFERENCES` - указатель на колонку строннюю таьлицу
+     - `REFERENCES` - указатель на колонку строннюю таблицу, либо FOREIGN KEY (company_name) REFERENCES company(name)
 - ### Drop
   - `DROP DATABASE company_repository;` - удаление БД
   - `DROP SCHEMA company_storage;` - удаление схемы в БД
--- examples/examples.sql
+++ examples/examples.sql
@@ -6,8 +6,8 @@ DROP TABLE company;
 DROP TABLE employee;
 CREATE TABLE company("id" INT GENERATED ALWAYS AS IDENTITY, "name" VARCHAR(128) UNIQUE NOT NULL, "date" DATE NOT NULL CHECK (date > '1995-01-01' AND date < '2020-01-01'), PRIMARY KEY (id) , UNIQUE(name, date));
 INSERT INTO company(name, date) VALUES ('Google', '2011-01-01'), ('Facebook', '2012-01-01');
-CREATE TABLE employee("id" INT GENERATED ALWAYS AS IDENTITY, first_name VARCHAR(128), last_name VARCHAR(128), salary INT, company_id INT references company (id), UNIQUE(first_name, last_name));
-INSERT INTO employee(first_name, last_name, salary) VALUES ('Ivan', 'Ivanov', 1000), ('Sveta', 'Svatikova', 2000), ('Petr', 'Petrov', 3000);
+CREATE TABLE employee("id" INT GENERATED ALWAYS AS IDENTITY, first_name VARCHAR(128), last_name VARCHAR(128), salary INT, company_id INT references company (id), company_name VARCHAR(128), UNIQUE(first_name, last_name), FOREIGN KEY (company_name) references company(name) ON DELETE cascade);
+INSERT INTO employee(first_name, last_name, salary) VALUES ('Ivan', 'Ivanov', 1000 ), ('Sveta', 'Svatikova', 2000), ('Petr', 'Petrov', 3000);
 INSERT INTO employee(first_name, last_name, salary) VALUES ('Ivan', 'Sidorov', 1000)
 select distinct first_name as fname from employee order by fname desc;
 select first_name as fname, last_name as lname, salary from employee order by salary desc limit 3 offset 1;
@@ -17,3 +17,13 @@ SELECT sum(salary) AS sum, avg(salary) AS avg, max(salary) AS max, min(salary) A
 SELECT lower(first_name) AS fname, upper(last_name) as lname, concat(first_name, ' ', last_name) FROM employee;
 SELECT * FROM company;
 SELECT * FROM employee;
+
+DROP TABLE company_storage.company;
+DROP TABLE company_storage.employee;
+
+CREATE TABLE company_storage.company("id" INT GENERATED ALWAYS AS IDENTITY, "name" VARCHAR(128) UNIQUE NOT NULL, "date" DATE NOT NULL CHECK (date > '1995-01-01' AND date < '2020-01-01'), PRIMARY KEY (id) , UNIQUE(name, date));
+INSERT INTO company_storage.company(name, date) VALUES ('Google', '2011-01-01'), ('Facebook', '2012-01-01');
+CREATE TABLE company_storage.employee("id" INT GENERATED ALWAYS AS IDENTITY, first_name VARCHAR(128), last_name VARCHAR(128), salary INT, company_id INT references company_storage.company(id), company_name VARCHAR(128), UNIQUE(first_name, last_name), FOREIGN KEY (company_name) references company_storage.company(name) ON DELETE cascade);
+INSERT INTO company_storage.employee(first_name, last_name, salary, company_id, company_name) VALUES ('Ivan', 'Ivanov', 1000, 1, 'Google' ), ('Sveta', 'Svatikova', 2000, 2, 'Facebook'), ('Petr', 'Petrov', 3000, 1, 'Google');
+
+
